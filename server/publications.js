3324c18c3137377be3401d5dbdb802098a7c24e0
kburts <kevin.burton2@gmail.com>
2015-02-21 06:34:45 +0000

    Updated notepad schema (Added: created/updated by modified at, hidden.)

--- server/publications.js
+++ server/publications.js
@@ -1,7 +1,12 @@
 // Notepad Publications.
 
+// Probably really shouldn't use this but going to put it in just in case for now..
 Meteor.publish('allNotepads', function() {
-  return Notepad.find();
+  return Notepad.find({hidden: false});
+});
+
+Meteor.publish('recentNotepads', function() {
+  return Notepad.find({hidden: false}, {sort: {modified: -1}, limit: 10});
 });
 
 Meteor.publish('notepad', function(id) {
