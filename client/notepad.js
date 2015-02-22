4f83c305a69591ffc20b0d6343a2c3cdcfcef3f7
kburts <kevin.burton2@gmail.com>
2015-02-22 01:59:14 +0000

    Added functionality for owners to update their own notebooks (title and hidden).

--- client/notepad.js
+++ client/notepad.js
@@ -8,6 +8,18 @@ function updateNotepad(notepad) {
 }
 
 
+Template.notepad.helpers({
+  isNotepadOwner: function () {
+    var notepad = Iron.controller().data().currNotepad;
+    if (!Meteor.user() || !notepad.created_by)
+      return false;
+    if (notepad.created_by._id == Meteor.user()._id)
+      return true;
+    console.log(Iron.controller().data().currNotepad.created_by, Meteor.user());
+    return false;
+  }
+});
+
 Session.set("countdown", false);
 
 // Events
@@ -15,6 +27,7 @@ Template.notepad.events({
   'keypress #notepad': function (event, template) {
     var controller = Iron.controller();
     var notepad = controller.data().currNotepad;
+    notepad.text = event.currentTarget.value;
 
     if (Session.get('countdown') == false) {
       Session.set('countdown', true);
@@ -22,9 +35,21 @@ Template.notepad.events({
         function () {
           updateNotepad(notepad);
           Session.set('countdown', false);
-          console.log('updating on timeout.');
-        }, 2000
+        }, 200
       );
     }
+  },
+  'submit #update-as-owner': function (event, template) {
+    event.preventDefault();
+
+    var title = event.target.title.value;
+    var hidden = event.target.hidden.checked;
+    var notepadId = Iron.controller().data().currNotepad._id;
+
+    console.log(title, hidden);
+    Meteor.call('updateNotepadAsOwner', {title: title, hidden: hidden, _id: notepadId});
+    
+    // Prevent default submit.
+    return false;
   }
 });
